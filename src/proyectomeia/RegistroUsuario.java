b899ffc2273259bae1361ae8503e5e01f0d663be
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-09-29 20:49:58 -0600

    fisih

--- src/proyectomeia/RegistroUsuario.java
+++ src/proyectomeia/RegistroUsuario.java
@@ -6,6 +6,8 @@
 package proyectomeia;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedList;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -226,15 +228,18 @@ public class RegistroUsuario extends javax.swing.JFrame {
        if(usuariosIngresados!=0){
            if(!VerSiExisteUsuario(usuario)){
                //Escribir en el archivo
-               String textoAnterior=Lector.Obtener("C:/MEIA/usuario.txt");
-               Escritor.Escribir("C:/MEIA/usuario.txt", textoAnterior+usuario.toString());
+               String textoAnterior=Lector.Obtener("C:/MEIA/bitacora_usuario.txt");
+               usuario.setRol(0);
+               
+               Escritor.Escribir("C:/MEIA/bitacora_usuario.txt", textoAnterior+usuario.toString());
                usuariosIngresados++;
            }
            else{
                //Mostrar que ya existe
            }
        }else{
-           Escritor.Escribir("C:/MEIA/usuario.txt",usuario.toString());
+           usuario.setRol(1);
+           Escritor.Escribir("C:/MEIA/bitacora_usuario.txt",usuario.toString());
            usuariosIngresados++;
        }
         
@@ -297,6 +302,43 @@ public class RegistroUsuario extends javax.swing.JFrame {
             return true;
         }        
     }
+    public static LinkedList<Usuario> obtenerUsuarios(int OpcionObtener){
+        String contenido="";
+        if(OpcionObtener==1){
+             contenido=Lector.Obtener("C:/MEIA/bitacora_usuario.txt");
+        }
+        else if(OpcionObtener==2){
+            contenido=Lector.Obtener("C:/MEIA/usuario.txt");
+        }
+        
+        String[] usuarios=contenido.split(",");
+        LinkedList<Usuario> users=new LinkedList<>();
+        for (int i = 0; i < usuarios.length; i++){
+            String[] camposUsuario=usuarios[i].split("|");
+            Usuario usuarioObtenido=new Usuario(camposUsuario[0],camposUsuario[1],camposUsuario[2],camposUsuario[3],camposUsuario[4],camposUsuario[5],camposUsuario[6],Integer.parseInt(camposUsuario[7]),Integer.parseInt(camposUsuario[8]));
+            usuarioObtenido.setRol(Integer.parseInt(camposUsuario[9]));
+            users.add(usuarioObtenido);
+        }
+        return users;
+    }
+    public static void rellenarUsuarios(LinkedList<Usuario> usuarios){
+        String textoEscribir="";
+        for(Usuario user:usuarios){
+            textoEscribir+=user.toString()+",";
+        }
+        Escritor.Escribir("C:/MEIA/usuario.txt", textoEscribir);
+    }
+    /*public static DescriptorUsuario obtenerDescriptorUsuario(int OpcionObtener){
+          String contenido="";
+        if(OpcionObtener==1){
+            contenido=Lector.Obtener("C:/MEIA/desc_usuario.txt");
+        }
+        else if(OpcionObtener==2){
+            contenido=Lector.Obtener("C:/MEIA/desc_usuariobitacora.txt");
+        }
+        String[] campos=contenido.split("|");
+      
+    }*///Metodo en proceso, falta terminar
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.toedter.calendar.JDateChooser CalendarFecha;
