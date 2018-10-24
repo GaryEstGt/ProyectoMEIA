ca9ab8b46acbb01ba10174266b25d9c4708dd4b1
Luis Farfán <luise.prado99@hotmail.com>
2018-10-23 18:08:26 -0600

    Eliminar

--- src/proyectomeia/ManejoUsuariosEnListas.java
+++ src/proyectomeia/ManejoUsuariosEnListas.java
@@ -55,6 +55,17 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         btnAsociarUsuario = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        txtUsuarioBuscar = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        btnBuscar = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstListasAsociadas = new javax.swing.JList<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        lstListasCreadas = new javax.swing.JList<>();
+        lblNombreUsuario = new javax.swing.JLabel();
         jPanel5 = new javax.swing.JPanel();
         cmbUsuario1 = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
@@ -76,7 +87,7 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
 
         cmbUsuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -139,15 +150,74 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Agregar un usuario", jPanel1);
 
+        jLabel7.setText("Ingrese el nombre del usuario que desea buscar");
+
+        btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
+
+        jLabel8.setText("Nombre de usuario:");
+
+        jLabel9.setText("Asociado a:");
+
+        jLabel10.setText("Listas creadas:");
+
+        jScrollPane1.setViewportView(lstListasAsociadas);
+
+        jScrollPane2.setViewportView(lstListasCreadas);
+
+        lblNombreUsuario.setText("Nombre");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 488, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(txtUsuarioBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                        .addComponent(btnBuscar))
+                    .addComponent(jScrollPane2)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel10)
+                            .addComponent(jLabel9))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addGap(18, 18, 18)
+                        .addComponent(lblNombreUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 244, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtUsuarioBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7)
+                    .addComponent(btnBuscar))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(lblNombreUsuario))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel10)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Buscar un usuario", jPanel2);
@@ -168,7 +238,7 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
 
         jLabel5.setText("Seleccione la lista");
 
-        jLabel6.setText("Seleccione el usuario que desea asociar");
+        jLabel6.setText("Seleccione el usuario que desea eliminar");
 
         btnDesasociarUsuario.setText("Eliminar usuario");
         btnDesasociarUsuario.addActionListener(new java.awt.event.ActionListener() {
@@ -195,7 +265,7 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
                             .addComponent(jLabel5)
                             .addComponent(jLabel6)))
                     .addComponent(btnDesasociarUsuario))
-                .addContainerGap(51, Short.MAX_VALUE))
+                .addContainerGap(49, Short.MAX_VALUE))
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -243,7 +313,8 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(btnRegresar))
+                .addComponent(btnRegresar)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -342,6 +413,16 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
             //LlenarComboboxUsuarioAsociado1();        
     }//GEN-LAST:event_cmbLista1ActionPerformed
 
+    private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+        // TODO add your handling code here:
+        if(!txtUsuarioBuscar.getText().toString().isEmpty()){
+            
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Debe ingresar el nombre de usuario");
+        }
+    }//GEN-LAST:event_btnBuscarActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -426,6 +507,7 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAsociarUsuario;
+    private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnDesasociarUsuario;
     private javax.swing.JButton btnRegresar;
     private javax.swing.JComboBox<String> cmbLista;
@@ -435,16 +517,26 @@ public class ManejoUsuariosEnListas extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cmbUsuarioAsociado;
     private javax.swing.JComboBox<String> cmbUsuarioAsociado1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JLabel lblNombreUsuario;
+    private javax.swing.JList<String> lstListasAsociadas;
+    private javax.swing.JList<String> lstListasCreadas;
+    private javax.swing.JTextField txtUsuarioBuscar;
     // End of variables declaration//GEN-END:variables
 }
 
