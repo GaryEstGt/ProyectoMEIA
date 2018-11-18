fb6247b58c7b99152922280dc0fcbdafd6f07c1c
Luis Farfán <luise.prado99@hotmail.com>
2018-11-17 22:46:02 -0600

    Final

--- src/proyectomeia/ArchivoArbolBinario.java
+++ src/proyectomeia/ArchivoArbolBinario.java
@@ -77,6 +77,208 @@ public class ArchivoArbolBinario {
         rellenarDescriptorCorreo(descriptorCorreo);
         rellenarNodos(nodos);
     }
+    
+    public static void eliminarCorreo(Correo correo){        
+        DescriptorCorreo descriptorCorreo = obtenerDescriptorCorreo();
+        LinkedList<Nodo> nodos = new LinkedList<Nodo>();
+        Nodo nodo = null;
+        
+        if(obtenerNodos() != null){
+            nodos = obtenerNodos();
+        }        
+        
+        if(nodos.size() > 0){
+            for (int i = 0; i < nodos.size(); i++) {
+                if(nodos.get(i).getCorreo().Compare(correo) == 0){
+                    nodo = nodos.get(i);
+                    break;
+                }
+            }
+        }        
+        
+        boolean tieneNodoDerecha = nodo.getDerecho() != -1 ? true : false;
+        boolean tieneNodoIzquierda = nodo.getIzquierdo() != -1 ? true : false;
+        
+        if (!tieneNodoDerecha && !tieneNodoIzquierda)
+            {
+                removeNodoCaso1(nodo, nodos);
+            }
+
+
+            if (tieneNodoDerecha && !tieneNodoIzquierda)
+            {
+                removeNodoCaso2(nodo, nodos);
+            }
+
+
+            if (!tieneNodoDerecha && tieneNodoIzquierda)
+            {
+                removeNodoCaso2(nodo, nodos);
+            }
+
+            /* Caso 3: Tiene ambos hijos */
+            if (tieneNodoDerecha && tieneNodoIzquierda)
+            {
+                removeNodoCaso3(nodo, nodos);
+            }                        
+    }
+    
+    protected static void removeNodoCaso1(Nodo nodo, LinkedList<Nodo> nodos)
+        {       
+            DescriptorCorreo descriptorCorreo = obtenerDescriptorCorreo();
+            Nodo Padre = null;
+            
+            boolean derecho = false;
+            
+            for (int i = 0; i < nodos.size(); i++) {
+                if(nodos.get(i).getDerecho() == nodo.getPosicion()){
+                    derecho = true;
+                    Padre = nodos.get(i);
+                }
+                else if(nodos.get(i).getIzquierdo() == nodo.getPosicion()){
+                    derecho = false;
+                    Padre = nodos.get(i);
+                }
+            }
+            
+            if (nodo.getPosicion() != descriptorCorreo.getRaiz())
+            {
+                if (derecho)
+                    Padre.setDerecho(-1);
+                else
+                    Padre.setIzquierdo(-1);
+            }
+            else
+            {
+                descriptorCorreo.setRaiz(0);
+            }
+        
+            nodos.get(nodo.getPosicion() - 1).getCorreo().setEstatus(0);
+            descriptorCorreo.setActivos(descriptorCorreo.getActivos() - 1);
+            descriptorCorreo.setInactivos(descriptorCorreo.getInactivos() + 1);        
+            rellenarDescriptorCorreo(descriptorCorreo);
+            rellenarNodos(nodos);
+        }
+    
+        protected static void removeNodoCaso2(Nodo nodo, LinkedList<Nodo> nodos)
+        {
+            DescriptorCorreo descriptorCorreo = obtenerDescriptorCorreo();
+            Nodo Padre = null;
+            
+            boolean derecho = false;
+            
+            for (int i = 0; i < nodos.size(); i++) {
+                if(nodos.get(i).getDerecho() == nodo.getPosicion()){
+                    derecho = true;
+                    Padre = nodos.get(i);
+                }
+                else if(nodos.get(i).getIzquierdo() == nodo.getPosicion()){
+                    derecho = false;
+                    Padre = nodos.get(i);
+                }
+            }
+            
+            if (nodo.getPosicion() != descriptorCorreo.getRaiz())
+            {                
+                if (nodo.getDerecho() != -1)
+                {                    
+                    if (derecho)
+                        Padre.setDerecho(nodo.getDerecho());
+                    else
+                        Padre.setIzquierdo(nodo.getDerecho());
+                }
+                else
+                {                    
+                    if (derecho)
+                        Padre.setDerecho(nodo.getIzquierdo());
+                    else
+                        Padre.setIzquierdo(nodo.getIzquierdo());
+                }
+            }
+            else
+            {
+                if (nodo.getDerecho() != -1)
+                {
+                    descriptorCorreo.setRaiz(nodo.getDerecho());                    
+                }
+                else
+                {
+                    descriptorCorreo.setRaiz(nodo.getIzquierdo());
+                }
+            }
+            
+            nodos.get(nodo.getPosicion() - 1).getCorreo().setEstatus(0);
+            descriptorCorreo.setActivos(descriptorCorreo.getActivos() - 1);
+            descriptorCorreo.setInactivos(descriptorCorreo.getInactivos() + 1);        
+            rellenarDescriptorCorreo(descriptorCorreo);
+            rellenarNodos(nodos);
+        }
+        
+        protected static void removeNodoCaso3(Nodo nodo, LinkedList<Nodo> nodos)
+        {
+            DescriptorCorreo descriptorCorreo = obtenerDescriptorCorreo();                        
+            Nodo aux = nodos.get(nodo.getIzquierdo() - 1);
+            Nodo Padre = null;
+            
+            boolean derecho = false;
+            
+            for (int i = 0; i < nodos.size(); i++) {
+                if(nodos.get(i).getDerecho() == nodo.getPosicion()){
+                    derecho = true;
+                    Padre = nodos.get(i);
+                }
+                else if(nodos.get(i).getIzquierdo() == nodo.getPosicion()){
+                    derecho = false;
+                    Padre = nodos.get(i);
+                }
+            }
+
+            while (aux.getDerecho() != -1)
+            {
+                Padre = aux;
+                aux = nodos.get(aux.getDerecho() - 1);                                
+            }
+
+            if (nodo.getDerecho() != aux.getPosicion() && nodo.getIzquierdo() != aux.getPosicion())
+            {
+                for (int i = 0; i < nodos.size(); i++) {
+                    if(nodos.get(i).getDerecho() == aux.getPosicion()){
+                        nodos.get(i).setDerecho(aux.getIzquierdo());
+                        break;
+                    }
+                }                
+            }
+            else
+            {
+                for (int i = 0; i < nodos.size(); i++) {
+                    if(nodos.get(i).getDerecho() == aux.getPosicion()){
+                        nodos.get(i).setIzquierdo(aux.getIzquierdo());
+                        break;
+                    }
+                }                             
+            }                            
+
+            aux.setDerecho(nodo.getDerecho());            
+            aux.setIzquierdo(nodo.getIzquierdo());                        
+
+            if (nodo.getPosicion() != descriptorCorreo.getRaiz())
+            {                                
+                if (derecho)
+                    Padre.setDerecho(aux.getPosicion());
+                else
+                    Padre.setIzquierdo(aux.getPosicion());
+            }
+            else
+            {                
+                descriptorCorreo.setRaiz(aux.getPosicion());
+            }
+            
+            nodos.get(nodo.getPosicion() - 1).getCorreo().setEstatus(0);
+            descriptorCorreo.setActivos(descriptorCorreo.getActivos() - 1);
+            descriptorCorreo.setInactivos(descriptorCorreo.getInactivos() + 1);        
+            rellenarDescriptorCorreo(descriptorCorreo);
+            rellenarNodos(nodos);
+        }
                 
     public static LinkedList<Nodo> obtenerNodos(){
         String contenido="";        
@@ -114,6 +316,5 @@ public class ArchivoArbolBinario {
     
     public static void rellenarDescriptorCorreo(DescriptorCorreo descriptor){        
         Escritor.Escribir("C:/MEIA/desc_correos.txt", descriptor.toString());                        
-    }
-    
+    }            
 }
